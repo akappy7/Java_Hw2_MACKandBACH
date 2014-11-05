0c57249c73a8095f77be27cfc09817c8ac9c0825
Steven Mackey <stevester94@gmail.com>
2014-11-05 01:04:33 -0800

    implimented all methods, array out of bounds on genRetAr. Premodify Seq.java commit

--- Given/part7/Plus.java
+++ Given/part7/Plus.java
@@ -31,8 +31,52 @@ public class Plus {
 		return new Jumble(retAr);
 	}
 
+	public static Seq plus (Constant c1, Delta d1) {
+		int smaller = Math.min(d1.num, d1.num);
+		return new Delta(smaller, d1.initial + c1.value, d1.delta);
+	}
+	
+	public static Seq plus (Delta d1, Constant c1) {
+		return plus(c1, d1);
+	}
 
 
+	public static Seq plus (Constant a, Jumble b) {
+		return genJumble(a, b);
+	}
+	public static Seq plus (Jumble a, Constant b) {
+		return genJumble(a, b);
+	}
+	public static Seq plus (Delta a, Jumble b) {
+		return genJumble(a, b);
+	}
+	public static Seq plus (Jumble a, Delta b) {
+		return genJumble(a, b);
+	}
+
+
+	private static Jumble genJumble(Seq s1, Seq s2) {
+		return new Jumble(calcSumAr(s1, s2));
+	}
+
+	private static int[] calcSumAr(Seq s1, Seq s2) {
+		SeqIt it1 = s1.createSeqIt();
+		SeqIt it2 = s2.createSeqIt();
+		int i = 0;
+		int retAr[] = new int[Math.min(s1.num, s2.num)];
+		if(retAr.length == 0) {
+			System.out.println("0 length");
+			System.exit(1);
+		}
+		//want the return array to be the size of the smaller sequence
+
+		while(it1.hasNext() && it2.hasNext()) {
+			retAr[i] = it1.next() + it2.next();
+			i++;
+		}
+
+		return retAr;
+	}
 }
 	//this may come in handy for part 8
 	// public static Seq plus (Seq c1, Seq c2) {
